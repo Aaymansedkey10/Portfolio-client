9a8b19105f5f1104855930ab329f1433505a61b7
Aaymansedkey10 <aymansedkey10@gmail.com>
2026-03-10 09:52:52 +0200

    remove undependenciespackages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
             <CustomCursor />
             <NavigationBar />
             {children}
-            {/* <ButtonsTop /> */}
           </QueryProvider>
         </ThemeProvider>
       </body>
